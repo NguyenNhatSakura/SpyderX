2fb25e15f6195b123ac2e086c0a34995418dc624
Nguyen Minh Nhat <minhtientiny@gmail.com>
2025-05-07 20:00:20 +0700

    Update main.lua

--- ScriptHub/UI/SpyderXLibV2/main.lua
+++ ScriptHub/UI/SpyderXLibV2/main.lua
@@ -124,7 +124,7 @@ function ChiuMyLy:Notify(desc)
 	Image.BackgroundTransparency = 1;
 	Image.Position = UDim2.new(0, 8, 0, 8);
 	Image.Size = UDim2.new(0, 45, 0, 45);
-	Image.Image = "rbxassetid://13940080072";
+	Image.Image = "rbxassetid://103733198839928";
 	Title.Parent = Frame;
 	Title.BackgroundColor3 = _G.Primary;
 	Title.BackgroundTransparency = 1;
